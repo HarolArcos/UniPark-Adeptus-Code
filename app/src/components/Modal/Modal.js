ad62d352ce654abc0e8a924de8a4c870118db63a
neidallm <neida.llusco1999@gmail.com>
2023-04-26 22:50:28 -0400

    add componente FormClient
    add formClient

---
+++ app/src/components/Modal/Modal.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { Modal as BootstrapModal,Button } from "react-bootstrap";
+
+const Modal = ({ mostrarModal ,hide,title,contend,asunto}) => {
+
+  return (
+    <BootstrapModal 
+    show = {mostrarModal}  
+    onHide={hide}
+    size="xl"
+    aria-labelledby="contained-modal-title-vcenter" 
+    fullscreen =' md-down'
+    >
+      <BootstrapModal.Header closeButton>
+        <BootstrapModal.Title >{title}</BootstrapModal.Title>
+      </BootstrapModal.Header>
+      <BootstrapModal.Body>
+        {contend}
+      </BootstrapModal.Body>
+      {/* <BootstrapModal.Footer>
+      <Button variant="secondary" onClick={hide}>
+            Cancelar
+          </Button>
+          <Button variant="primary" >
+            {asunto}
+          </Button>
+      </BootstrapModal.Footer> */}
+    </BootstrapModal>
+  );
+};
+
+export default Modal;
--- app/src/components/Router/AppRouter.js
+++ app/src/components/Router/AppRouter.js
@@ -8,6 +8,7 @@ import React from 'react'
 import Login from "../Login/Login";
 import { Main } from "../Main/Main";
 import { Contact } from "../Contact/Contact";
+import { Client } from "../Client/Client";
 
 
 export const AppRouter = () => {
@@ -17,6 +18,7 @@ export const AppRouter = () => {
                 <Routes>
                     <Route exact={true} path='/login' element={<Login></Login>} ></Route>
                     <Route exact={true} path='/main' element={<Main></Main>} ></Route>
+                    <Route exact={true} path='/client' element={<Client></Client>} ></Route>
                 </Routes>
         </Router>
     )
