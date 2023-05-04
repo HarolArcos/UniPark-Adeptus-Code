dda068d90324e62a6718d68e398835ec8dca91df
GonzaZeballos <101687891+GonzaZeballos@users.noreply.github.com>
2023-05-04 14:39:09 -0400

    apiMessage

---
+++ ADEPTUSCODE-BackEnd/app/apiMessage/autoLoad.php
@@ -0,0 +1,9 @@
+<?php
+    include_once ('../apiMessage/config/config.php');
+    include_once('../../lib/common/apiJson.php');
+    include_once('../../lib/db/dbpg.php');
+    include_once('../../lib/common/log.php');
+
+    include_once('../../lib/coreo/message/message.php');
+
+?>
