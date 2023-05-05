8dc5f2e6fdfb09afc3a601c6c45f743e791fe286
Harol Nelson Arcos Terrazas <harolarcos.ha@gmail.com>
2023-05-04 20:43:51 -0400

    ApiReferences + references
    creacion de la API REFERENCES que hace consultas a la tabla "referencia"

---
+++ ADEPTUSCODE-BackEnd/app/apiReference/apiReference.php
@@ -0,0 +1,249 @@
+<?php
+    include('autoload.php'); 
+    $HTTP_RAW_POST_DATA = file_get_contents("php://input");
+    $HTTP_RAW_POST_DATA = (json_decode($HTTP_RAW_POST_DATA)) ? $HTTP_RAW_POST_DATA : '';
+    $HTTP_RAW_POST_DATA = (empty($HTTP_RAW_POST_DATA)) ? json_encode(array_merge($_REQUEST, $_FILES)) : $HTTP_RAW_POST_DATA;
+    $server = new apiJson($HTTP_RAW_POST_DATA);
+    $server->Register("insertReference");
+    $server->Register("listReferences");
+    $server->Register("test");
+    $server->Register("test2");
+    $server->Register("editReference");
+    //$server->Register("changeStatePerson");
+    $server->start();
+
+    function insertReference($arg){
+        $options = array('path' => LOGPATH,'filename' => FILENAME);
+        $startTime = microtime(true);
+        $_db=new dataBasePG(CONNECTION);
+        $_log = new log($options);
+        $respValidate = validateArg($arg);  
+        if($respValidate){
+            $arrLog = array("input"=>$arg,"output"=>$arg);
+            $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+            $_log->notice($mensaje);
+            return $respValidate;
+        }
+
+        $errorlist=array();
+        $tableNameReference = "";
+        $nameSpaceReference = ""; 
+        $descriptionReference = "";
+        $valueReference = "";
+        $statusReference = "";
+        if(isset($arg->tableNameReference)){
+            $tableNameReference =  $arg->tableNameReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro tableNameReference");
+        }
+        if(isset($arg->nameSpaceReference)){
+            $nameSpaceReference =  $arg->nameSpaceReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro nameSpaceReference");
+        }
+        if(isset($arg->descriptionReference)){
+                $descriptionReference =  $arg->descriptionReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro descriptionReference");
+        }
+        if(isset($arg->valueReference)){
+            $valueReference =  $arg->valueReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro valueReference");
+        }
+        if(isset($arg->statusReference)){
+            $statusReference =  $arg->statusReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro statusReference");
+        }
+        if(count($errorlist)!==0){
+            return array("codError" => 200, "data" => array("desError"=>$errorlist)); 
+        }
+        $tableNameReference = $arg->tableNameReference ;
+        $nameSpaceReference = $arg->nameSpaceReference; 
+        $descriptionReference = $arg->descriptionReference;
+        $valueReference = $arg->valueReference;
+        $statusReference = $arg->statusReference;
+
+        $_reference = new reference($_db);
+        $responseInsert = $_reference->insertReferenceDb($tableNameReference,$nameSpaceReference,$descriptionReference,$valueReference,$statusReference);
+        if ( $responseInsert) {
+            $response = array("codError" => 200, "data" => array("desError"=>"Inserción exitosa"));
+        }else{
+            $response = array("codError" => 200, "data" => array("desError"=>"Inserción fallida"));
+        }
+        $timeProcess = microtime(true)-$startTime;
+        $arrLog = array("time"=>$timeProcess, "input"=>json_encode($arg),"output"=>$response);
+        $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+        $_log->notice($mensaje);
+        return $response;
+    }
+
+
+    function editReference($arg){
+        $options = array('path' => LOGPATH,'filename' => FILENAME);
+        $startTime = microtime(true);
+        $_db=new dataBasePG(CONNECTION);
+        $_log = new log($options);
+        $respValidate = validateArg($arg);  
+        if($respValidate){
+            $arrLog = array("input"=>$arg,"output"=>$arg);
+            $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+            $_log->notice($mensaje);
+            return $respValidate;
+        }
+        $errorlist=array();
+        $idReference =  "";
+        $tableNameReference = "";
+        $nameSpaceReference = ""; 
+        $descriptionReference = "";
+        $valueReference = "";
+        $statusReference = "";
+
+        if(isset($arg->idReference)){
+            $idReference =  $arg->idReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro idReference");
+        }
+        if(isset($arg->tableNameReference)){
+            $tableNameReference =  $arg->tableNameReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro tableNameReference");
+        }
+        if(isset($arg->nameSpaceReference)){
+            $nameSpaceReference =  $arg->nameSpaceReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro nameSpaceReference");
+        }
+        if(isset($arg->descriptionReference)){
+                $descriptionReference =  $arg->descriptionReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro descriptionReference");
+        }
+        if(isset($arg->valueReference)){
+            $valueReference =  $arg->valueReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro valueReference");
+        }
+        if(isset($arg->statusReference)){
+            $statusReference =  $arg->statusReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro statusReference");
+        }
+        if(count($errorlist)!==0){
+            return array("codError" => 200, "data" => array("desError"=>$errorlist)); 
+        }
+
+        $idReference =  $arg->idReference;
+        $tableNameReference = $arg->tableNameReference ;
+        $nameSpaceReference = $arg->nameSpaceReference; 
+        $descriptionReference = $arg->descriptionReference;
+        $valueReference = $arg->valueReference;
+        $statusReference = $arg->statusReference;
+
+        $_reference = new reference($_db);
+        $responseEdit = $_reference->editReferenceDb($idReference, $tableNameReference, $nameSpaceReference, $descriptionReference, $valueReference, $statusReference);
+        if ( $responseEdit) {
+            $response = array("codError" => 200, "data" => array("desError"=>"Cambios realizados con exito"));
+        }else{
+            $response = array("codError" => 200, "data" => array("desError"=>"Cambios fallidos"));
+        }
+
+        $timeProcess = microtime(true)-$startTime;
+        $arrLog = array("time"=>$timeProcess, "input"=>json_encode($arg),"output"=>$response);
+        $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+        $_log->notice($mensaje);
+        return $response;
+    }
+
+    function changeStateReference($arg){
+        $options = array('path' => LOGPATH,'filename' => FILENAME);
+        $startTime = microtime(true);
+        $_db=new dataBasePG(CONNECTION);
+        $_log = new log($options);
+        $respValidate = validateArg($arg);  
+        if($respValidate){
+            $arrLog = array("input"=>$arg,"output"=>$arg);
+            $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+            $_log->notice($mensaje);
+            return $respValidate;
+        }
+        $errorlist=array();
+        $idReference =  "";
+        $statusReference = "";
+        if(isset($arg->idReference)){
+            $idReference =  $arg->idReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro idReference");
+        }
+        if(isset($arg->statusReference)){
+            $statusReference =  $arg->statusReference;
+        }else{
+            array_push($errorlist,"Error: falta parametro statusReference");
+        }
+        if(count($errorlist)!==0){
+            return array("codError" => 200, "data" => array("desError"=>$errorlist));
+        }
+
+        $idReference =  $arg->idReference;
+        $statusReference = $arg->statusReference;
+
+        $_reference = new reference($_db);
+        $responseDelete = $_reference->changeStateReferenceDb($idReference, $statusReference);
+
+        if ( $responseDelete){
+            $response = array("codError" => 200, "data" => array("desError"=>"Cambio de estado exitosa"));
+        }else{
+            $response = array("codError" => 200, "data" => array("desError"=>"Cambio de estado fallida"));
+        }
+
+        $timeProcess = microtime(true)-$startTime;
+        $arrLog = array("time"=>$timeProcess, "input"=>json_encode($arg),"output"=>$response);
+        $mensaje = print_r($arrLog, true)." Funcion: ".__FUNCTION__;
+        $_log->notice($mensaje);
+        return $response;
+    }
+
+    function listReferences($arg){
+        $options = array('path' => LOGPATH,'filename' => FILENAME);
+        $_db=new dataBasePG(CONNECTION);
+        $_log = new log($options);
+       
+        $tableNameReference = $arg->tableNameReference ;
+        $nameSpaceReference = $arg->nameSpaceReference;
+
+        $_reference = new reference($_db);
+        $responseList = $_reference->listReferencesDb($tableNameReference, $nameSpaceReference);
+
+        if ( $responseList) {
+            $mensaje = "Se listo correctamente a las personas - Funcion: ".__FUNCTION__;
+            $_log->info($mensaje);
+        }else{
+            $response = array("codError" => 200, "data" => array("desError"=>"Listado fallido, es posible que no existan personas"));
+            $mensaje = "No se pudo listara a las personas - Funcion: ".__FUNCTION__;
+            $_log->error($mensaje);
+            return $response;
+        }
+        
+        return $responseList;
+    }
+
+    function validateArg($arg){
+        $resp = false;
+        if (!is_object($arg)) {
+            $resp = array("codError" => 400, "data" =>array("desError"=>"Error en parametros") );
+        }
+        if (is_null($arg)) {
+            $resp = array("codError" => 400, "data" =>array("desError"=>"Error parametros vacios"));
+        }
+        return $resp;
+    }
+    
+    function test($arg) {    
+        return array("codError" => 200, "data" => $arg);
+    }
+
+    function test2($arg) {    
+        return array("codError" => 200, "data" => "Hola estamos en linea");
+    }
+    
---
+++ ADEPTUSCODE-BackEnd/lib/coreo/reference/reference.php
@@ -0,0 +1,235 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT']."/UniPark-Adeptus-Code/ADEPTUSCODE-BackEnd/lib/common/log.php");  
+//Clase Creada 04/05/2023
+//by: Harol Arcos
+//Clase mapeada de la tabla referencia
+
+class reference {
+
+    private $optionsLog;
+    private $_db;
+    private $idReference,$tableNameReference,$nameSpaceReference, $descriptionReference,$valueReference,$statusReference;
+
+    function __construct($_db,$idReference=0){
+        
+        $this->_db=$_db;
+        if ($idReference!=0) {
+            $this->setPerson($idReference);
+        }
+    }
+
+    function __destruct(){
+        unset($this->_db);
+        unset($this->idReference);
+        unset($this->tableNameReference);
+        unset($this->nameSpaceReference);
+        unset($this->descriptionReference);
+        unset($this->valueReference);
+        unset($this->statusReference);
+    }
+
+    private function createLog($fileName, $logMessage, $tipeError){
+        $this->optionsLog = array(
+            'path'           => $_SERVER['DOCUMENT_ROOT']."/UniPark-Adeptus-Code/ADEPTUSCODE-BackEnd/log/nucleo/reference",           
+            'filename'       => $fileName,);
+          $_log = new log($this->optionsLog);
+          
+          switch ($tipeError) {
+            case "info":
+                $_log->info($logMessage);
+              break;
+            case "notice":
+                $_log->notice($logMessage);
+              break;
+            case "warning":
+                $_log->warning($logMessage);
+              break;
+            case "error":
+                $_log->error($logMessage);
+              break;
+            case "critical":
+                $_log->critical($logMessage);
+              break;
+            case "alert":
+                $_log->alert($logMessage);
+              break;
+            case "emergency":
+                $_log->emergency($logMessage);
+              break;
+            case "gau":
+                $_log->gau($logMessage);
+              break;
+            case "debug":
+                $_log->debug($logMessage);
+                break;
+            default:
+                $_log->info($logMessage);
+                break;
+          }
+    }
+    
+    private function setReference($idReference){
+        $response = FALSE;
+        $dataReference = $this->findReferenceDb($idReference);
+        if($dataReference){
+            $this->mapReference($dataReference);
+            $response = TRUE;
+        }
+        $arrLog = array("input"=>$idReference,"output"=>$response);
+        $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "warning");
+        return $response;
+    }
+    
+    private function findReferenceDb($idReference){
+        $response = false;
+        $sql =  "SELECT * FROM referencia ".
+                "where referencia_id = $idReference";
+        $rs = $this->_db->query($sql);
+        if($this->_db->getLastError()) {
+            $arrLog = array("input"=>$idReference,"sql"=>$sql,"error"=>$this->_db->getLastError());
+           $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "error");   
+        } else {
+            $response = $rs[0];
+            $arrLog = array("input"=>$idReference,"output"=>$response,"sql"=>$sql);
+            $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "debug");
+        }
+        return $response;
+    }
+
+    public function insertReferenceDb($tableNameReference,$nameSpaceReference, $descriptionReference,$valueReference,$statusReference){
+        $response = false;
+        $sql =  "INSERT INTO referencia(referencia_nombre_tabla,referencia_nombre_campo,referencia_descripcion,referencia_valor,referencia_estado)
+         VALUES ('$tableNameReference', '$nameSpaceReference', '$descriptionReference', '$valueReference', '$statusReference')";
+        $rs = $this->_db->query($sql);
+        if($this->_db->getLastError()) {
+            
+            $arrLog = array("input"=>array( "tableNameReference"=> $tableNameReference,
+                                            "nameSpaceReference"=> $nameSpaceReference,
+                                            "descriptionReference"=> $descriptionReference, 
+                                            "valueReference"=> $valueReference,
+                                            "statusReference"=> $statusReference),
+                            "sql"=>$sql,
+                            "error"=>$this->_db->getLastError());
+            $this->createLog('dbLog', print_r($arrLog, true), "error");  
+        } else {
+            $response = $rs;
+            $arrLog = array("input"=>array( "tableNameReference"=> $tableNameReference,
+                                            "nameSpaceReference"=> $nameSpaceReference,
+                                            "descriptionReference"=> $descriptionReference, 
+                                            "valueReference"=> $valueReference,
+                                            "statusReference"=> $statusReference),
+                            "output"=>$response,
+                            "sql"=>$sql);
+
+            $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "debug");
+        }
+        return $response;
+    }
+
+    public function editReferenceDb($idReference, $tableNameReference, $nameSpaceReference, $descriptionReference, $valueReference, $statusReference){
+        $response = false;
+        $sql =  "UPDATE referencia SET 
+        referencia_nombre_tabla = '$tableNameReference',
+        referencia_nombre_campo = '$nameSpaceReference',
+        referencia_descripcion = '$descriptionReference',
+        referencia_valor = '$valueReference',
+        referencia_estado = '$statusReference'
+      WHERE referencia_id = $idReference";
+        $rs = $this->_db->query($sql);
+        if($this->_db->getLastError()) {
+            
+            $arrLog = array("input"=>array( "idReference" => $idReference,
+                                            "tableNameReference"=> $tableNameReference,
+                                            "nameSpaceReference"=> $nameSpaceReference,
+                                            "descriptionReference"=> $descriptionReference, 
+                                            "valueReference"=> $valueReference,
+                                            "statusReference"=> $statusReference),
+                            "sql"=>$sql,
+                            "error"=>$this->_db->getLastError());
+            $this->createLog('dbLog', print_r($arrLog, true), "error");  
+        } else {
+            $response = $rs;
+            $arrLog = array("input"=>array( "idReference" => $idReference,
+                                            "tableNameReference"=> $tableNameReference,
+                                            "nameSpaceReference"=> $nameSpaceReference,
+                                            "descriptionReference"=> $descriptionReference, 
+                                            "valueReference"=> $valueReference,
+                                            "statusReference"=> $statusReference),
+                            "output"=>$response,
+                            "sql"=>$sql);
+            $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "debug");
+        }
+        return $response;
+    }
+
+    public function changeStateReferenceDb($idReference, $statusReference){
+        $response = false;
+        $sql =  "UPDATE referencia SET referencia_estado = $statusReference WHERE referencia_id = $idReferencia";
+        $rs = $this->_db->query($sql);
+        if($this->_db->getLastError()) {
+            $arrLog = array("input"=>array( "idReference" => $idReference,"referencia_estado" => $statusReference),
+                            "sql"=>$sql,
+                            "error"=>$this->_db->getLastError());
+            $this->createLog('dbLog', print_r($arrLog, true), "error");  
+        } else {
+            $response = $rs;
+            $arrLog = array("input"=>array( "idReference" => $idReference,"referencia_estado" => $statusReference),
+                            "output"=>$response,
+                            "sql"=>$sql);
+            $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "debug");
+        }
+        return $response;
+    }
+
+    public function listReferencesDb($tableNameReference, $nameSpaceReference){
+        $response = false;
+        $sql =  "SELECT * FROM referencia WHERE referencia_nombre_tabla = '$tableNameReference' AND referencia_nombre_campo = '$nameSpaceReference'";
+        $rs = $this->_db->select($sql);
+        if($this->_db->getLastError()) {
+            $arrLog = array(
+                            "sql"=>$sql,
+                            "error"=>$this->_db->getLastError());
+            $this->createLog('dbLog', print_r($arrLog, true), "error");  
+        } else {
+            $response = $rs;
+            $arrLog = array(
+                            "output"=>$response,
+                            "sql"=>$sql);
+            $this->createLog('apiLog', print_r($arrLog, true)." Function error: ".__FUNCTION__, "debug");
+        }
+        return $response;
+    }
+
+    private function mapReferencia($rs){
+        $this->idReference = $rs['referencia_id'];
+        $this->tableNameReference = $rs['referencia_nombre_tabla'];
+        $this->nameSpaceReference = $rs['referencia_nombre_campo'];
+        $this->descriptionReference = $rs['referencia_descripcion'];
+        $this->valueReference = $rs['referencia_valor'];
+        $this->statusReference = $rs['referencia_estado'];
+    }
+
+    public function getReferenciaId(){
+        return $this->idReference;
+    }
+
+    public function getReferenciaNombreTabla(){
+        return $this->tableNameReference;
+    }
+    
+    public function getReferenciaNombreCampo(){
+        return $this->nameSpaceReference;
+    }
+    
+    public function getReferenciaDescripcion(){
+        return $this->descriptionReference;
+    } 
+    
+    public function getReferenciaValor(){
+        return $this->valueReference;
+    }
+
+    public function getReferenciaEstado(){
+        return $this->statusReference;
+    }
+}
