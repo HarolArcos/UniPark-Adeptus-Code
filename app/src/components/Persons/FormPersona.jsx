bd4a1ddb2f7c7e3272780991e44fed7ab24d27a2
RodriC98374 <rodricesp.3.0@gmail.com>
2023-05-09 18:17:23 -0400

    feature: Aniadidos los campos faltantes en el modal para agregar personas

--- app/src/components/Persons/FormPersona.jsx
+++ app/src/components/Persons/FormPersona.jsx
@@ -3,8 +3,9 @@ import React, { useEffect } from "react";
 import { Form, Button,Modal } from "react-bootstrap";
 import {Formik, ErrorMessage } from 'formik';
 import { useFetchSendData } from "../../hooks/HookFetchSendData";
+import "./FormPersona.css";
 
-const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo}) => {
+const FormularioPersona = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo}) => {
 
   const {data,fetchData} = useFetchSendData();
   
@@ -21,13 +22,21 @@ const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo})
         fnamePerson: persona.persona_nombre,
         lnamePerson: persona.persona_apellido,
         cIPerson: persona.persona_ci,
-        phonePerson: persona.persona_telefono
+        phonePerson: persona.persona_telefono,
+        telegramPerson: persona.persona_telegram,
+        statePerson: persona.persona_estado,
+        nickPerson: persona.persona_nickname,
+        pwPersona: persona.persona_contraseña
       }:{
         idVehiculo: '1',
         fnamePerson: '',
         lnamePerson: '',
         cIPerson: '',
-        phonePerson: ''
+        phonePerson: '',
+        telegramPerson: '',
+        statePerson: '',
+        nickPerson: '',
+        pwPersona: ''
       }}
     
     validate={values => {
@@ -37,7 +46,7 @@ const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo})
         errors.fnamePerson ='El campo es requerido';
       }
       else if(!/^[A-Za-z]+$/i.test(values.fnamePerson)){
-        errors.plateVehicle ='caracteres invalidos'
+        errors.fnamePerson ='caracteres invalidos'
       }
 
       if(!values.lnamePerson){
@@ -59,6 +68,34 @@ const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo})
       }
       else if(!/^[0-9]+$/i.test(values.phonePerson)){
         errors.phonePerson ='datos invalidos'
+      }
+
+      if(!values.telegramPerson){
+        errors.telegramPerson ='El campo es requerido';
+      }
+      else if(!/^[A-Za-z-0-9]+$/i.test(values.telegramPerson)){
+        errors.telegramPerson ='datos invalidos'
+      }
+
+      if(!values.statePerson){
+        errors.statePerson ='El campo es requerido';
+      }
+      else if(!/^[0-9]+$/i.test(values.statePerson)){
+        errors.statePerson ='datos invalidos'
+      }
+      
+      if(!values.nickPerson){
+        errors.nickPerson ='El campo es requerido';
+      }
+      else if(!/^[A-Za-z-0-9]+$/i.test(values.nickPerson)){
+        errors.nickPerson ='datos invalidos'
+      }
+
+      if(!values.pwPersona){
+        errors.pwPersona ='El campo es requerido';
+      }
+      else if(!/^[A-Za-z-0-9]+$/i.test(values.pwPersona)){
+        errors.pwPersona ='datos invalidos'
       }
       return errors;
     }}
@@ -83,55 +120,107 @@ const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo})
       {({values,errors,handleBlur,handleChange,handleSubmit})=>(
         <Form  className="container">
       <div className="row ">
-        <div className="col-md-4">
+        <div className="col-md-12 contentModalPerson">
 
-          <Form.Group controlId="fnamePerson">
-            <Form.Label className="text-left">Nombre</Form.Label>
-            <Form.Control 
-            type="text" 
-            name="fnamePerson"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.fnamePerson}
-            />
-          </Form.Group>
-          <ErrorMessage name="fnamePerson" component={()=>(<div className="text-danger">{errors.fnamePerson}</div>)}></ErrorMessage>
-          
-          <Form.Group controlId="lnamePerson">
-            <Form.Label>Apellido</Form.Label>
-            <Form.Control 
-            type="text"
-            name="lnamePerson"
-            onChange={handleChange}
-            onBlur={handleBlur} 
-            value={values.lnamePerson} 
-            />
-          </Form.Group>
-          <ErrorMessage name="lnamePerson" component={()=>(<div className="text-danger">{errors.lnamePerson}</div>)}></ErrorMessage>
-          
-          <Form.Group controlId="cIPerson">
-            <Form.Label>CI</Form.Label>
-            <Form.Control 
-            type="text" 
-            name="cIPerson"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.cIPerson}
-            />
-          </Form.Group>
-          <ErrorMessage name="cIPerson" component={()=>(<div className="text-danger">{errors.cIPerson}</div>)}></ErrorMessage>
-          
-          <Form.Group controlId="phonePerson">
-            <Form.Label>Telefono</Form.Label>
-            <Form.Control 
-            type="text" 
-            name="phonePerson"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.phonePerson}
-            />
-          </Form.Group>
-          <ErrorMessage name="phonePerson" component={()=>(<div className="text-danger">{errors.phonePerson}</div>)}></ErrorMessage>
+          <div className="column">
+            <Form.Group controlId="fnamePerson">
+              <Form.Label className="label text-left">Nombre</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="fnamePerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.fnamePerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="fnamePerson" component={()=>(<div className="text-danger">{errors.fnamePerson}</div>)}></ErrorMessage>
+            
+            <Form.Group controlId="lnamePerson">
+              <Form.Label className="label" >Apellido</Form.Label>
+              <Form.Control 
+              type="text"
+              name="lnamePerson"
+              onChange={handleChange}
+              onBlur={handleBlur} 
+              value={values.lnamePerson} 
+              />
+            </Form.Group>
+            <ErrorMessage name="lnamePerson" component={()=>(<div className="text-danger">{errors.lnamePerson}</div>)}></ErrorMessage>
+            
+            <Form.Group controlId="cIPerson">
+              <Form.Label className="label" >CI</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="cIPerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.cIPerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="cIPerson" component={()=>(<div className="text-danger">{errors.cIPerson}</div>)}></ErrorMessage>
+            
+            <Form.Group controlId="phonePerson">
+              <Form.Label className="label" >Telefono</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="phonePerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.phonePerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="phonePerson" component={()=>(<div className="text-danger">{errors.phonePerson}</div>)}></ErrorMessage>
+          </div>
+
+          <div className="column">
+            <Form.Group controlId="telegramPerson">
+              <Form.Label className="label">Telegram</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="telegramPerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.telegramPerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="telegramPerson" component={()=>(<div className="text-danger">{errors.telegramPerson}</div>)}></ErrorMessage>
+
+            <Form.Group controlId="statePerson">
+              <Form.Label className="label" >Estado Persona</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="statePerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.statePerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="statePerson" component={()=>(<div className="text-danger">{errors.statePerson}</div>)}></ErrorMessage>
+
+            <Form.Group controlId="nickPerson">
+              <Form.Label className="label" >Nickname</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="nickPerson"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.nickPerson}
+              />
+            </Form.Group>
+            <ErrorMessage name="nickPerson" component={()=>(<div className="text-danger">{errors.nickPerson}</div>)}></ErrorMessage>
+
+            <Form.Group controlId="pwPersona">
+              <Form.Label className="label">Contraseña</Form.Label>
+              <Form.Control 
+              type="text" 
+              name="pwPersona"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.pwPersona}
+              />
+            </Form.Group>
+            <ErrorMessage name="pwPersona" component={()=>(<div className="text-danger">{errors.pwPersona}</div>)}></ErrorMessage>
+          </div>
         </div>
 
       </div>
@@ -150,4 +239,4 @@ const Formulario = ({asunto,cancelar, persona,actualizarVehiculo, añadirNuevo})
   );
 };
 
-export default Formulario;
+export default FormularioPersona;
