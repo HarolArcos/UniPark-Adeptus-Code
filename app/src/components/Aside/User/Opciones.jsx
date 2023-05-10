bd9f64fb2a9407b1324b95fb610ed1be9be712a0
JorgeBeltr <jorgedbo183@gmail.com>
2023-05-10 00:15:25 -0400

    actualizacion

--- app/src/components/Aside/User/Opciones.jsx
+++ app/src/components/Aside/User/Opciones.jsx
@@ -1,18 +1,20 @@
+import { useContext } from "react"
 
+import { DataUser } from '../../context/UserContext';
 const parse = require("html-react-parser");
 
 let bdopcion = require("./Options.json").opcionesuser;
 
-let primarylist = bdopcion.filter((padres) => padres.padre === "");
 
 export default function Opcions() {
-
+  const {userglobal} = useContext(DataUser)
+  let primarylist = bdopcion.filter((padres) => padres.padre === ""&& padres.tipo>=userglobal.persona_tipo);
 
   return primarylist.map((register) => {
     let string = "";
 
     let secontlist1 = bdopcion.filter(
-      (secontlist) => secontlist.padre === register.orden
+      (secontlist) => secontlist.padre === register.orden && secontlist.tipo>=userglobal.persona_tipo
     );
     secontlist1.map((secont) => (string = string + secont.componente));
 
