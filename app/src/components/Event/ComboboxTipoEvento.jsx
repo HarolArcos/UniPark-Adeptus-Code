9dd3edc3dc64cae013751156b971bed85383b998
neidallm <neida.llusco1999@gmail.com>
2023-06-12 13:20:06 -0400

    add_changes
    addCambios

--- app/src/components/Event/ComboboxTipoEvento.jsx
+++ app/src/components/Event/ComboboxTipoEvento.jsx
@@ -1,33 +1,4 @@
-// import Select from 'react-select'; 
-// import { useState } from 'react';
 
-// export default function ComboboxPlacas({ onPersonaIdChange ,id}) { // actualiza la firma para recibir el id y la función onPersonaIdChange
-
-//   const options = [
-//     { value: 15, label: "Ingreso"},
-//     { value: 16, label: "Salida" }
-//     ];
-//     const [selectedTypeId, setSelectedTypeId] = useState(null);
-
-//     const handleTypeEventChange = (vehSelectedOption) => {
-//         setSelectedTypeId(vehSelectedOption.value);
-//         console.log(vehSelectedOption);
-//     };
-
-//     const types = options.map((veh) => ({value: veh.value, label: veh.label }));
-//     const defaultValue = options.find( veh => veh. )
-
-//     return (
-//       <div className="propietario">
-//         <Select 
-//             options={types}
-//             value={types.find(option => option.value === selectedTypeId)}
-
-//             onChange={handleTypeEventChange}
-//         />
-//       </div>
-//     );
-// }
 
 import React, { useEffect , useState} from "react";
 import Select from "react-select";
